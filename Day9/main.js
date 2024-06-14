58db7b5108c71d0f5cf28201158d79c2913ad1bd
*romaisa <*mahamkhurram1230@gmail.com>
2024-06-14 17:44:51 +0500

    update

---
+++ Day9/main.js
@@ -0,0 +1,56 @@
+// //Day 9
+// //Q25
+var alien_colour1 = "green";
+if (alien_colour1 == "green") {
+    console.log("earn 5 points");
+}
+alien_colour1 = "red";
+if (alien_colour1 == "green") {
+    console.log("no output");
+}
+//Q26
+var alien_colour2 = "green";
+if (alien_colour2 == "green") {
+    console.log("earn 5 points");
+}
+else {
+    console.log("earn 10 points");
+}
+alien_colour2 = "red";
+if (alien_colour2 == "green") {
+    console.log("earn 5 points");
+}
+else {
+    console.log("earn 10 points");
+}
+//Q27
+var alien_colour = "green";
+if (alien_colour == "green") {
+    console.log("earn 5 points");
+}
+else if (alien_colour == "red") {
+    console.log("earn 10 points");
+}
+else if (alien_colour == "yellow") {
+    console.log("earn 15 points");
+}
+alien_colour = "red";
+if (alien_colour == "green") {
+    console.log("earn 5 points");
+}
+else if (alien_colour == "red") {
+    console.log("earn 10 points");
+}
+else if (alien_colour == "yellow") {
+    console.log("earn 15 points");
+}
+alien_colour = "yellow";
+if (alien_colour == "green") {
+    console.log("earn 5 points");
+}
+else if (alien_colour == "red") {
+    console.log("earn 10 points");
+}
+else if (alien_colour == "yellow") {
+    console.log("earn 15 points");
+}
