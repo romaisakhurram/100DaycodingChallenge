5fd681c8becc2386da67098a2af9165bdff89307
*romaisa <*mahamkhurram1230@gmail.com>
2024-06-14 17:24:07 +0500

    update

--- Day7/main.ts
+++ Day7/main.ts
@@ -2,7 +2,7 @@
 //Day 7
 //Q19
 let dinnerGuests : string[] =["Saima" , "Ali" , "Uzair" , "Maham"]
-console.log(`you can invite ${dinnerGuests.length} person to invite dinner`)
+console.log(`you can invite ${dinnerGuests.length} person to invite dinner`);
  
 //Q20
 let language : string[] = ["Urdu" , "English" , "Punjabi" , "Persian"]
@@ -12,7 +12,7 @@ console.log(language);
 let book  : {name : string, author : string , yearpublished : number } =
  {name : "a beuty icon" ,
  author : "x,y,z" ,  
- yearpublished : 1945}
+ yearpublished : 1945};
 
  console.log(`a book name ${book.name}, author name ${book.author} and
   year published${book.yearpublished}`);
