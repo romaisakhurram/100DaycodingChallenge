09c499c147dbb5082a555e2f4fd8761164c446d6
*romaisa <*mahamkhurram1230@gmail.com>
2024-06-14 16:47:21 +0500

    update

---
+++ Day3/main.js
@@ -0,0 +1,9 @@
+//DAY :3
+//Q7 and Q8
+console.log(5 + 3); // addition
+console.log(10 - 2); // subtraction
+console.log(2 * 4); // multiplication
+console.log(16 / 2); // division
+// Q9
+var favouriteNumber = 6;
+console.log("my favourite number is ".concat(favouriteNumber));
---
+++ Day3/main.ts
@@ -0,0 +1,11 @@
+
+//DAY :3
+//Q7 and Q8
+console.log(5 + 3); // addition
+console.log(10 - 2); // subtraction
+console.log(2 * 4); // multiplication
+console.log(16 / 2); // division
+
+// Q9
+let favouriteNumber : number = 6
+console.log(`my favourite number is ${favouriteNumber}`);
