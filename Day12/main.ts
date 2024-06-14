c5c027a222bf94130e0b390a4cb2480ea7bcca62
*romaisa <*mahamkhurram1230@gmail.com>
2024-06-14 17:56:15 +0500

    update

--- Day12/main.ts
+++ Day12/main.ts
@@ -1,14 +1,14 @@
 
 //DAy 12
 //Q34
-let pizzas = ["peproni" , "fajita , chicken"]
+let pizzas : string[] = ["peproni" , "fajita , chicken"]
 pizzas.forEach((pizza)=>{
     console.log(`I like ${pizza} pizza`);
     console.log("I love pizza");  
 })
 
 //Q35
-let animals = ["dog" , " cat" , "rabbit"]
+let animals : string[] = ["dog" , " cat" , "rabbit"]
 animals.forEach((animal) => {
 console.log(`${animal} would make a great pet`);
 console.log("both animal are sharp legs");
